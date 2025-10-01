db1f1c73595f5992135c75c85dab419eea342ea0
Blessed-HubX <mynamewendel@gmail.com>
2025-10-01 20:45:31 -0300

    Plants-Vs-Brainrots.lua

---
+++ Plants-Vs-Brainrots.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 127742093697776 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/PLANTSVSBRAINROT.lua"))()
+end
