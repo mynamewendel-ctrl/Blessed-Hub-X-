2592e71fb965b3caffb7917959ff8a8f1551221e
Blessed-HubX <mynamewendel@gmail.com>
2025-10-03 17:50:34 -0300

    MySingingBrainrot.lua

--- MySingingBrainrot.lua
+++ MySingingBrainrot.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 89343390950953 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/FishItUpdate2.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/MyBrainrotOBF.lua"))()
 end
