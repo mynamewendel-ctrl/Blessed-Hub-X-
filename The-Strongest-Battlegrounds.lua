715b293ef4d57d053528eb3bc53fda511115a28a
Blessed-HubX <mynamewendel@gmail.com>
2025-10-08 20:55:18 -0300

    The-Strongest-Battlegrounds.lua

---
+++ The-Strongest-Battlegrounds.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 10449761463 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/PlantsVsBrainrotPrisonEvent.lua"))()
+end
