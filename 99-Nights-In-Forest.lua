a4a444efd7a348d04a062902a16bcc88c58db7f8
Blessed-HubX <mynamewendel@gmail.com>
2025-10-16 19:14:47 -0300

    Remove conditional check for game.PlaceId

--- 99-Nights-In-Forest.lua
+++ 99-Nights-In-Forest.lua
@@ -1,3 +1,1 @@
-if game.PlaceId == 7326934954 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/99nightLoader.lua"))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/99nightLoader.lua"))()
