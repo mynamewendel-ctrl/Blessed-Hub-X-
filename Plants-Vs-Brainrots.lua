c5c2f1f965fbe156cfd849270a2cd69a8d5f70fa
Blessed-HubX <mynamewendel@gmail.com>
2025-10-20 14:38:19 -0300

    Update script loading for game PlaceId 127742093697776

--- Plants-Vs-Brainrots.lua
+++ Plants-Vs-Brainrots.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 127742093697776 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/246c796470ca98ca.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/547434ca07d18eab.lua"))()
 end
