1b3510ce285f9a6ad0135c449ee186d84bef3585
Blessed-HubX <mynamewendel@gmail.com>
2025-11-12 22:22:56 -0300

    Update script loading URL in Plants-Vs-Brainrots.lua

--- Plants-Vs-Brainrots.lua
+++ Plants-Vs-Brainrots.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 127742093697776 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/moonveil.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/mynamewendel-ctrl/Blessed-Hub-X-/refs/heads/main/PvbUpd12.lua"))()
 end
